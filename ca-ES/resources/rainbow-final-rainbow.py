c93d6bbe4bf5624c31549495b2eed98ac1145b25
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2020-04-16 15:56:41 +0100

    New translations rainbow-final-rainbow.py (Catalan)

--- ca-ES/resources/rainbow-final-rainbow.py
+++ ca-ES/resources/rainbow-final-rainbow.py
@@ -6,17 +6,17 @@ import time
 sense = SenseHat()
 sense.clear()
 
-R = [255, 0, 0]  # red
-O = [255, 165, 0]  # orange
-Y = [255, 255, 0] # yellow
-G = [0, 255, 0] # green
-B = [0, 55, 155] # blue
-I = [25, 0, 255] # indigo
-V = [255, 0, 255] # violet
-X = [0, 0, 0]  # off
+R = [255, 0, 0]  # vermell
+O = [255, 165, 0]  # taronja
+Y = [255, 255, 0] # groc
+G = [0, 255, 0] # verd
+B = [0, 55, 155] # blau
+I = [25, 0, 255] # indi
+V = [255, 0, 255] # violeta
+X = [0, 0, 0]  # apagat
 
 
-rainbow = [
+arcsantmarti = [
 R, R, R, R, R, R, R, R,
 R, O, O, O, O, O, O, O,
 R, O, Y, Y, Y, Y, Y, Y,
