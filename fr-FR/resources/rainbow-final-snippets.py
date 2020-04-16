9b946b59e170199994774d04b0affddfaf19ae69
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2020-04-16 14:26:17 +0100

    New translations rainbow-final-snippets.py (French)

--- fr-FR/resources/rainbow-final-snippets.py
+++ fr-FR/resources/rainbow-final-snippets.py
@@ -1,10 +1,10 @@
-rainbow = [
+arcenciel = [
 R, R, R, R, R, R, R, R,
 R, O, O, O, O, O, O, O,
-R, O, Y, Y, Y, Y, Y, Y,
-R, O, Y, G, G, G, G, G,
-R, O, Y, G, B, B, B, B,
-R, O, Y, G, B, I, I, I,
-R, O, Y, G, B, I, V, V,                                                                   
-R, O, Y, G, B, I, V, X
+R, O, J, J, J, J, J, J,
+R, O, J, V, V, V, V, V,
+R, O, J, V, B, B, B, B,
+R, O, J, V, B, I, I, I,
+R, O, J, V, B, I, M, M,                                                                   
+R, O, J, V, B, I, M, X
 ]
