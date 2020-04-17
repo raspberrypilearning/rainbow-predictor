e04fddaa20c403e8e104251ed4a972659a1942d2
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2020-04-17 13:16:59 +0100

    New translations rainbow-final-snippets.py (Romanian)

--- ro-RO/resources/rainbow-final-snippets.py
+++ ro-RO/resources/rainbow-final-snippets.py
@@ -1,10 +1,10 @@
-rainbow = [
+curcubeu = [
 R, R, R, R, R, R, R, R,
 R, O, O, O, O, O, O, O,
-R, O, Y, Y, Y, Y, Y, Y,
-R, O, Y, G, G, G, G, G,
-R, O, Y, G, B, B, B, B,
-R, O, Y, G, B, I, I, I,
-R, O, Y, G, B, I, V, V,                                                                   
-R, O, Y, G, B, I, V, X
+R, O, G, G, G, G, G, G,
+R, O, G, V, V, V, V, V,
+R, O, G, V, A, A, A, A,
+R, O, G, V, A, I, I, I,
+R, O, G, V, A, I, M, M,                                                                   
+R, O, G, V, A, I, M, X
 ]
