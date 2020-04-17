6c0963a1d90aa2bb653750990da86f3da3efee32
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2020-04-17 13:17:00 +0100

    New translations rainbow-final-rainbow.py (Romanian)

--- ro-RO/resources/rainbow-final-rainbow.py
+++ ro-RO/resources/rainbow-final-rainbow.py
@@ -6,25 +6,25 @@ import time
 sense = SenseHat()
 sense.clear()
 
-R = [255, 0, 0]  # red
-O = [255, 165, 0]  # orange
-Y = [255, 255, 0] # yellow
-G = [0, 255, 0] # green
-B = [0, 55, 155] # blue
+R = [255, 0, 0]  # roșu
+O = [255, 165, 0]  # oranj
+G = [255, 255, 0] # galben
+V = [0, 255, 0] # verde
+A = [0, 55, 155] # albastru
 I = [25, 0, 255] # indigo
-V = [255, 0, 255] # violet
-X = [0, 0, 0]  # off
+M = [255, 0, 255] # mov
+X = [0, 0, 0] # oprit
 
 
-rainbow = [
+curcubeu = [
 R, R, R, R, R, R, R, R,
 R, O, O, O, O, O, O, O,
-R, O, Y, Y, Y, Y, Y, Y,
-R, O, Y, G, G, G, G, G,
-R, O, Y, G, B, B, B, B,
-R, O, Y, G, B, I, I, I,
-R, O, Y, G, B, I, V, V,                                                                   
-R, O, Y, G, B, I, V, X
+R, O, G, G, G, G, G, G,
+R, O, G, V, V, V, V, V,
+R, O, G, V, A, A, A, A,
+R, O, G, V, A, I, I, I,
+R, O, G, V, A, I, M, M,                                                                   
+R, O, G, V, A, I, M, X
 ]
 
 while True:
@@ -34,7 +34,7 @@ while True:
   if sense.humidity > 80 and sense.temp > 20:
     sense.set_pixels(rainbow)
   elif sense.humidity > 80 and sense.temp < 0 :
-    sense.clear([255, 255, 255]) # white snow
+    sense.clear([255, 255, 255]) # zăpadă albă
   elif sense.humidity <= 80 and sense.temp > 20 :
     sense.clear([255, 255, 0]) # yellow sun
   else:
