7e7e0cdf431f1bb896d3c2efdffc33a4a6290f97
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2020-04-17 07:55:45 +0100

    New translations rainbow-final-rainbow.py (Hindi)

--- hi-IN/resources/rainbow-final-rainbow.py
+++ hi-IN/resources/rainbow-final-rainbow.py
@@ -6,17 +6,17 @@ import time
 sense = SenseHat()
 sense.clear()
 
-R = [255, 0, 0]  # red
-O = [255, 165, 0]  # orange
-Y = [255, 255, 0] # yellow
-G = [0, 255, 0] # green
-B = [0, 55, 155] # blue
-I = [25, 0, 255] # indigo
-V = [255, 0, 255] # violet
-X = [0, 0, 0]  # off
+R = [255, 0, 0]  # red (लाल)
+O = [255, 165, 0]  # orange (नारंगी)
+Y = [255, 255, 0] # yellow (पीला)
+G = [0, 255, 0] # green (हरा)
+B = [0, 55, 155] # blue (नीला)
+I = [25, 0, 255] # indigo (नील)
+V = [255, 0, 255] # violet (वायलेट)
+X = [0, 0, 0]  # off (ऑफ)
 
 
-rainbow = [
+इंद्रधनुष = [
 R, R, R, R, R, R, R, R,
 R, O, O, O, O, O, O, O,
 R, O, Y, Y, Y, Y, Y, Y,
@@ -32,7 +32,7 @@ while True:
   h = t.tm_hour
 
   if sense.humidity > 80 and sense.temp > 20:
-    sense.set_pixels(rainbow)
+    sense.set_pixels(इंद्रधनुष)
   elif sense.humidity > 80 and sense.temp < 0 :
     sense.clear([255, 255, 255]) # white snow
   elif sense.humidity <= 80 and sense.temp > 20 :
