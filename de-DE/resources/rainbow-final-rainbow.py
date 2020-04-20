a9dbaf31be86d0458d0c94a820b7238a5b442630
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2020-04-20 08:42:44 +0100

    New translations rainbow-final-rainbow.py (German)

--- de-DE/resources/rainbow-final-rainbow.py
+++ de-DE/resources/rainbow-final-rainbow.py
@@ -3,40 +3,40 @@
 from sense_hat import SenseHat
 import time
 
-sense = SenseHat()
-sense.clear()
+Sensor = SenseHat()
+Sensor.clear()
 
-R = [255, 0, 0]  # red
-O = [255, 165, 0]  # orange
-Y = [255, 255, 0] # yellow
-G = [0, 255, 0] # green
-B = [0, 55, 155] # blue
-I = [25, 0, 255] # indigo
-V = [255, 0, 255] # violet
-X = [0, 0, 0]  # off
+R = [255, 0, 0]  # Rot
+O = [255, 165, 0]  # Orange
+Gb = [255, 255, 0] # Gelb
+Gn = [0, 255, 0] # Grün
+B = [0, 55, 155] # Blau
+I = [25, 0, 255] # Indigo
+V = [255, 0, 255] # Violett
+X = [0, 0, 0] # aus
 
 
-rainbow = [
+Regenbogen = [
 R, R, R, R, R, R, R, R,
 R, O, O, O, O, O, O, O,
-R, O, Y, Y, Y, Y, Y, Y,
-R, O, Y, G, G, G, G, G,
-R, O, Y, G, B, B, B, B,
-R, O, Y, G, B, I, I, I,
-R, O, Y, G, B, I, V, V,                                                                   
-R, O, Y, G, B, I, V, X
+R, O, Gb, Gb, Gb, Gb, Gb, Gb,
+R, O, Gb, Gn, Gn, Gn, Gn, Gn,
+R, O, Gb, Gn, B, B, B, B,
+R, O, Gb, Gn, B, I, I, I,
+R, O, Gb, Gn, B, I, V, V,                                                                   
+R, O, Gb, Gn, B, I, V, X
 ]
 
 while True:
-  t = time.localtime() 
-  h = t.tm_hour
+  z = time.localtime() 
+  s = z.tm_hour
 
-  if sense.humidity > 80 and sense.temp > 20:
-    sense.set_pixels(rainbow)
-  elif sense.humidity > 80 and sense.temp < 0 :
-    sense.clear([255, 255, 255]) # white snow
-  elif sense.humidity <= 80 and sense.temp > 20 :
-    sense.clear([255, 255, 0]) # yellow sun
+  if Sensor.humidity > 80 and Sensor.temp > 20:
+    Sensor.set_pixels(Regenbogen)
+  elif Sensor.humidity > 80 and Sensor.temp < 0 :
+    Sensor.clear([255, 255, 255]) # weißer Schnee
+  elif Sensor.humidity <= 80 and Sensor.temp > 20 :
+    Sensor.clear([255, 255, 0]) # gelbe Sonne
   else:
     Sensor.clear()
     
