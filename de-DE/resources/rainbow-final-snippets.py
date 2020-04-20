9db71f16a31d914ae2609205adc09436709890c5
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2020-04-20 08:42:43 +0100

    New translations rainbow-final-snippets.py (German)

--- de-DE/resources/rainbow-final-snippets.py
+++ de-DE/resources/rainbow-final-snippets.py
@@ -1,10 +1,10 @@
-rainbow = [
+Regenbogen = [
 R, R, R, R, R, R, R, R,
 R, O, O, O, O, O, O, O,
-R, O, Y, Y, Y, Y, Y, Y,
-R, O, Y, G, G, G, G, G,
-R, O, Y, G, B, B, B, B,
-R, O, Y, G, B, I, I, I,
-R, O, Y, G, B, I, V, V,                                                                   
-R, O, Y, G, B, I, V, X
+R, O, Gb, Gb, Gb, Gb, Gb, Gb,
+R, O, Gb, Gn, Gn, Gn, Gn, Gn,
+R, O, Gb, Gn, B, B, B, B,
+R, O, Gb, Gn, B, I, I, I,
+R, O, Gb, Gn, B, I, V, V,                                                                   
+R, O, Gb, Gn, B, I, V, X
 ]
