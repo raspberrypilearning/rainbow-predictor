50e8e716c7f322ba1e7773619fe61591c9d6ed8f
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-09-15 08:56:16 +0100

    New translations rainbow-final-rainbow.py (Turkish)

--- tr-TR/resources/rainbow-final-rainbow.py
+++ tr-TR/resources/rainbow-final-rainbow.py
@@ -1,30 +1,30 @@
 #!/bin/python3
 
-from sense_hat import SenseHat
+sense_hat ithalat SenseHat
 import time
 
 sense = SenseHat()
 sense.clear()
 
-R = [255, 0, 0]  # red
-O = [255, 165, 0]  # orange
-Y = [255, 255, 0] # yellow
-G = [0, 255, 0] # green
-B = [0, 55, 155] # blue
-I = [25, 0, 255] # indigo
-V = [255, 0, 255] # violet
-X = [0, 0, 0]  # off
+K = [255, 0, 0] # kırmızı
+T = [255, 165, 0] # turuncu
+S = [255, 255, 0] # sarı
+Y = [0, 255, 0] # yeşil
+MA = [0, 55, 155] # mavi
+L = [25, 0, 255] # lacivert
+MO = [255, 0, 255] # mor
+X = [0, 0, 0] # kapalı
 
 
-rainbow = [
-R, R, R, R, R, R, R, R,
-R, O, O, O, O, O, O, O,
-R, O, Y, Y, Y, Y, Y, Y,
-R, O, Y, G, G, G, G, G,
-R, O, Y, G, B, B, B, B,
-R, O, Y, G, B, I, I, I,
-R, O, Y, G, B, I, V, V,                                                                   
-R, O, Y, G, B, I, V, X
+gokkusagi = [
+K, K, K, K, K, K, K, K,
+K, T, T, T, T, T, T, T,
+K, T, S, S, S, S, S, S,
+K, T, S, Y, Y, Y, Y, Y,
+K, T, S, Y, MA, MA, MA, MA,
+K, T, S, Y, MA, L, L, L,
+K, T, S, Y, MA, L, MO, MO,                                                                   
+K, T, S, Y, MA, L, MO, X
 ]
 
 while True:
@@ -32,11 +32,11 @@ while True:
   h = t.tm_hour
 
   if sense.humidity > 80 and sense.temp > 20:
-    sense.set_pixels(rainbow)
+    sense.set_pixels(gokkusagi)
   elif sense.humidity > 80 and sense.temp < 0 :
-    sense.clear([255, 255, 255]) # white snow
+    sense.clear([255, 255, 255]) # kar beyazı
   elif sense.humidity <= 80 and sense.temp > 20 :
-    sense.clear([255, 255, 0]) # yellow sun
+    sense.clear([255, 255, 0]) # güneş sarısı
   else:
     sense.clear()
     
