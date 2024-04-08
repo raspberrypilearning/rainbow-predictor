a6d36e02ad571f914e88f89ad641c32b49eed676
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-04-08 15:59:48 +0100

    New translations rainbow-final-rainbow.py (Polish)

--- pl-PL/resources/rainbow-final-rainbow.py
+++ pl-PL/resources/rainbow-final-rainbow.py
@@ -6,30 +6,30 @@ import time
 sense = SenseHat ()
 sense.clear()
 
-C = [255, 0, 0] # czerwony
-P = [255, 165, 0] # pomarańczowy
-Z = [255, 255, 0] # żółty
-G = [0, 255, 0] # "Z" jest zajęte więc użyjmy "G"(ang. Green - zielony)
-N = [0, 55, 155] # niebieski
+R = [255, 0, 0] # czerwony
+O = [255, 165, 0] # pomarańczowy
+Y = [255, 255, 0] # żółty
+G = [0, 255, 0] # "Z" jest zajęte więc użyjmy "G"(ang.
+B = [0, 55, 155] # niebieski
 I = [25, 0, 255] # indygo
-F = [255, 0, 255] # fioletowy
+V = [255, 0, 255] # fioletowy
 X = [0, 0, 0] # wyłączony
 
 
 tecza = [
-C, C, C, C, C, C, C, C,
 C, P, P, P, P, P, P, P,
 C, P, Z, Z, Z, Z, Z, Z,
-C, P, Z, G, G, G, G, G,
-C, P, Z, G, N, N, N, N,
-C, P, Z, G, N, I, I, I,
-C, P, Z, G, N, I, F, F,                                                                   
+R, O, Y, Y, Y, Y, Y, Y,
+R, O, Y, G, G, G, G, G,
+R, O, Y, G, B, B, B, B,
+R, O, Y, G, B, I, I, I,
+R, O, Y, G, B, I, V, V,                                                                   
 C, P, Z, G, N, I, F, X
 ]
 
 while True:
   c = time.localtime () 
-  g = c.tm_hour
+  g = t.tm_hour
 
   if sense.humidity > 80 and sense.temp > 20:
     sense.set_pixels(tecza)
