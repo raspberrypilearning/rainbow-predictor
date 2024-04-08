f8419552af8c535293ef55a4ba361cf4f0d38345
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-04-08 16:00:05 +0100

    New translations rainbow-final-snippets.py (Polish)

--- pl-PL/resources/rainbow-final-snippets.py
+++ pl-PL/resources/rainbow-final-snippets.py
@@ -4,7 +4,7 @@ C, P, P, P, P, P, P, P,
 C, P, Z, Z, Z, Z, Z, Z,
 C, P, Z, G, G, G, G, G,
 C, P, Z, G, N, N, N, N,
-C, P, Z, G, N, I, I, I,
+R, O, Y, G, B, Ja, Ja, Ja,
 C, P, Z, G, N, I, F, F,                                                                   
 C, P, Z, G, N, I, F, X
 ]
